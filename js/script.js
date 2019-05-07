8dd62e5ed73d2858f42d482658a30e20af1c690a
fabiobressa <fabiobressa@gmail.com>
2019-05-07 00:08:40 -0300

    initial commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+var PayWithMyBankOptions = {
+    widgetContainerId: "widget-container-id"
+};
+
+var establishData = {
+    'accessId':'cev95HZaVSmke96tPdpp',
+    'merchantId':'100000008',
+    'description':'merchant desc',
+    'currency':'USD',
+    'amount':'1.00',
+    'merchantReference':'ref0001',
+    'paymentType':'Instant',
+    'metadata': {
+        'lang':'en'
+    },
+    'returnUrl':'#return',
+    'cancelUrl':'#cancel'
+};
+
+//PayWithMyBank.selectBankWidget(establishData, PayWithMyBankOptions);
+
